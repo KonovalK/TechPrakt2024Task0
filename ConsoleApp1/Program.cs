64b9389b782c8e6555d2d6d3a84d9aa8edd8d6a1
KonovalK <mr.kolyakonoval@gmail.com>
2024-01-28 23:44:41 +0200

    Console App created

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello!");
+        myFunc();
+    }
+    internal static void myFunc() 
+    {
+        Console.WriteLine($"Input a: ");
+        double a = double.Parse(Console.ReadLine());
+        Console.WriteLine($"Input b: ");
+        double b = double.Parse(Console.ReadLine());
+        Console.WriteLine($"{a} + {b} = {a+b}");
+    }
+}
